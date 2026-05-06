67c24020f8add8f97371b823c8b0ad44c48b557e
manishramdhave <73530199+manishramdhave@users.noreply.github.com>
2026-05-07 04:15:05 +0530

    Update TestController.java

--- src/main/java/com/velocity/restcontroller/TestController.java
+++ src/main/java/com/velocity/restcontroller/TestController.java
@@ -1,18 +1,24 @@
 package com.velocity.restcontroller;
 
+import java.util.Arrays;
+import java.util.List;
+import com.velocity.model.Order; // Import your model
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-	/*
-	 * Design restful web service to return "This is executable jar" message on
-	 * screen
-	 */
-
-	@GetMapping("/getOrder")
-	public String getOrder() {
-		return "This is Order Page";
-	}
+    /*
+     * Restful web service to return a list of Orders
+     */
+    @GetMapping("/getOrder")
+    public List<Order> getOrder() {
+        // Mocking a list of orders
+        return Arrays.asList(
+            new Order(101, "Laptop", 1200.50, "Shipped"),
+            new Order(102, "Smartphone", 800.00, "Processing"),
+            new Order(103, "Headphones", 150.00, "Delivered")
+        );
+    }
 }
